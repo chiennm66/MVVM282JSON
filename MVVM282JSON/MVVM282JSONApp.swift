e17758f4c5c1271254128dcfb7ac0c5ac504dffd
chienabc <chien97666@gmail.com>
2026-02-28 18:46:45 +0700

    Initial Commit

---
+++ MVVM282JSON/MVVM282JSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM282JSONApp.swift
+//  MVVM282JSON
+//
+//  Created by CHIEN on 28/2/26.
+//
+
+import SwiftUI
+
+@main
+struct MVVM282JSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

be06dec267f43490593c40abf77ac0a2d74c8e04
chienabc <chien97666@gmail.com>
2026-02-28 19:54:23 +0700

    Handle Models

---
+++ MVVM282JSON/Models/iPhone.swift
@@ -0,0 +1,8 @@
+//
+//  iPhone.swift
+//  MVVM282JSON
+//
+//  Created by CHIEN on 28/2/26.
+//
+
+import Foundation
---
+++ MVVM282JSON/Views/PhoneCard.swift
@@ -0,0 +1,8 @@
+//
+//  PhoneCard.swift
+//  MVVM282JSON
+//
+//  Created by CHIEN on 28/2/26.
+//
+
+import Foundation

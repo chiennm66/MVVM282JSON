7f84ef27897778f3419613e49326d985e6621669
chienabc <chien97666@gmail.com>
2026-02-28 19:58:50 +0700

    Models

--- MVVM282JSON/Models/iPhone.swift
+++ MVVM282JSON/Models/iPhone.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+
+struct iPhone: Codable, Identifiable, Hashable{
+    
+    let id :Int
+    let name :String
+    let tagline :String
+    let chip :String
+    let storage : [String]
+    let price: Double
+    let series :String
+    // có thể hỏi chỗ nào chưa hiểu
+    // code phần này vào trước. Nên code tay vào đừng copy
+    
+    
+    
+    var formattedPrice: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = Locale(identifier: "vi_VN")
+        return formatter.string(from: NSNumber(value: price)) ?? "\(price) đ"
+    }
+    
+}
